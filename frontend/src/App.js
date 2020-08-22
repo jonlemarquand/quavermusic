3dada76f2b09c7995584db98029db7fd66132aa7
Jon Le Marquand <jon.lemarquand@gmail.com>
2020-08-22 15:25:02 +0100

    Header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import logo from './assets/logo1000.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>
+      <img src={logo} className="logo-header" alt="Quaver Music"/>
+      <p className="subtitle">What have you been listening to?</p>
+    </header>
+    <footer>
+    <img src={logo} className="logo-footer" alt="Quaver Music"/>
+    <div className="footer-content">
+      About | Contact
+    </div>
+    </footer>
     </div>
   );
 }
